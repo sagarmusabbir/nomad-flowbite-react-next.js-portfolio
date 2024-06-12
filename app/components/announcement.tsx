f57af3dad398fd6c3afcd8903c7f17d63f79dbbc
MUSABBIR SAGAR <56739377+sagarmusabbir@users.noreply.github.com>
2024-06-12 23:00:09 +0600

    Update announcement.tsx

--- app/components/announcement.tsx
+++ app/components/announcement.tsx
@@ -7,13 +7,13 @@ const AnnouncementBanner: FC = function () {
     <Banner>
       <div className="flex w-full items-center justify-center border border-b border-gray-200 bg-gray-50 px-4 py-2 dark:border-gray-700 dark:bg-gray-700 bg-slate-50">
         <div className="mx-auto flex items-center gap-x-3">
-          <Badge color="dark" icon={MdFiberNew} className="w-fit md:inline">
+          <Badge color="gray" icon={MdFiberNew} className="md:inline">
           
           </Badge>
           <p className="text-sm font-medium text-gray-900 dark:text-white md:my-0">
             Tailwind Developer Portfolio Template is Out Now.
             <Badge
-              href="#" color="dark"
+              href="#" color="gray"
               className="ml-2 inline-flex items-center "
             >
               Check it out!
