c200e3c11ccd75b9b9ddd9dda73385581fa908e5
MUSABBIR SAGAR <56739377+sagarmusabbir@users.noreply.github.com>
2024-06-12 22:38:30 +0600

    Update alternate-sections.tsx

--- app/components/alternate-sections.tsx
+++ app/components/alternate-sections.tsx
@@ -7,7 +7,7 @@ const ProjectPortfolioAlternateSections: FC = function () {
       <div className="max-w-screen-xl px-4 py-8 mx-auto lg:px-6 sm:py-16 lg:py-24">
         <div className="max-w-2xl mx-auto text-center">
           <h2 className="text-3xl font-extrabold leading-tight tracking-tight text-gray-900 sm:text-4xl dark:text-white">
-            Our work
+            Latest Projects
           </h2>
           <p className="mt-4 text-base font-normal text-gray-500 sm:text-xl dark:text-gray-400">
             Flowbite helps you connect with friends, family and communities of
